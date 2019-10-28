b659dcc3ce0abd773e83585a8f585eb443ae1a0f
Thibault-GX <thibault.grimoux@gmail.com>
2019-10-28 10:51:51 +0100

    Push initial.

---
+++ javascript.js
@@ -0,0 +1,3 @@
+function displayMessage() {
+    alert("Mais euh... On remercie pas quelqu'un de sortir d'une input, ça n'a pas de sens. \nIl vaudrait mieux le remercier lorsqu'il valide son input.");
+}
